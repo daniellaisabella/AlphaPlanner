533683eb6ef6fad69b4742c33c4a3a173228af35
daniellaisabella <dato0001@stud.kea.dk>
2025-04-25 12:14:51 +0200

    controllers

--- src/main/java/org/example/alphaplanner/controller/UserController.java
+++ src/main/java/org/example/alphaplanner/controller/UserController.java
@@ -1,4 +1,18 @@
 package org.example.alphaplanner.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.example.alphaplanner.service.BaseService;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserController {
+    private BaseService service;
+
+    public UserController(BaseService service) {
+        this.service = service;
+    }
+
+    private boolean isLoggenIn(HttpSession session){
+        return session.getAttribute("user") !=null;
+    }
 }
