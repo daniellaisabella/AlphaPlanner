39167a8f6f209d614222709fc2f52aa5422d1139
Simonknu <sikn0002@stud.kea.dk>
2025-04-28 13:33:10 +0200

    first commit

--- src/main/java/org/example/alphaplanner/controller/TaskController.java
+++ src/main/java/org/example/alphaplanner/controller/TaskController.java
@@ -1,7 +1,9 @@
 package org.example.alphaplanner.controller;
 
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,7 +13,8 @@ public class TaskController {
 
 // dummy method to get into the task of a specific subproject
     @GetMapping("/showSub")
-    public String showSub(){
+    public String showSub(int sub_id, Model model){
+        model.addAttribute()
         return
     }
 //===========================================================
