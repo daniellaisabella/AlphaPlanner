c4a91336f36f0aff951ba34011fe5914ecf021ed
chrisHS03 <chso0005@stud.kea.dk>
2025-04-28 13:28:34 +0200

    Login, admin site

---
+++ src/main/java/org/example/alphaplanner/controller/AlphaPlannerExceptionHandler.java
@@ -0,0 +1,15 @@
+package org.example.alphaplanner.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class AlphaPlannerExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String handleIllegalArgument(IllegalArgumentException e, Model model) {
+        model.addAttribute("errorMessage", e.getMessage());
+        return "error";
+    }
+}
--- src/main/java/org/example/alphaplanner/service/BaseService.java
+++ src/main/java/org/example/alphaplanner/service/BaseService.java
@@ -1,7 +1,35 @@
 package org.example.alphaplanner.service;
 
+import org.example.alphaplanner.models.User;
+import org.example.alphaplanner.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BaseService {
+
+    private UserRepository userRepository;
+
+    public BaseService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    //------------ USER METHODS-----------//
+
+    public boolean login(User user) {
+        return userRepository.login(user);
+    }
+
+    public int getUserId(User user) {
+        return userRepository.getUserId(user);
+    }
+
+    public String getUserRole(Object userId) {
+        return userRepository.getUserRole(userId);
+    }
+
+    public List<User> getUsersByRole(String role) {
+        return userRepository.getUsersByRole(role);
+    }
 }
